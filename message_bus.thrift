b0cffb6efecf58e2d5c34e953d0385485d251b59
ashley <ashchou3@gmail.com>
2016-01-27 15:28:43 -0800

    Remove service and split subscription notification into two. Also move to separate thrift_types directory.

---
+++ message_bus.thrift
@@ -0,0 +1,25 @@
+struct Variable {
+  1: string name
+  2: string value
+  3: i64 timestamp
+  4: bool externally_settable
+}
+
+struct Peripheral {
+  1: string name
+  2: map<string, Variable> variables
+}
+
+struct Device {
+  1: string name
+  2: map<string, Peripheral> peripherals
+}
+
+struct PeripheralSubscriptionNotification {
+  1: string device_name
+  2: Peripheral updated_peripheral
+}
+
+struct DeviceSubscriptionNotification {
+  1: Device updated_device
+}
