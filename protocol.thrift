c81fd694085dcdd3ee54cc51ab1ada7b65082ac4
ashley <ashchou3@gmail.com>
2016-02-04 16:32:35 -0800

    Add protocol message types and delimiter to thrift

--- protocol.thrift
+++ protocol.thrift
@@ -1,3 +1,8 @@
+const string DELIMITER = " "
+const string MSG_TYPE_REQ = "req"
+const string MSG_TYPE_RESP = "resp"
+const string MSG_TYPE_NOTIF = "notif"
+
 enum StatusCode {
   NO_ERROR = 0,
   BAD_ARGS = 1
