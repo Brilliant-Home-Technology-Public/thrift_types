64f64bd7ec2fa57243be121400809efb96b49d70
Jeremy Hiatt <jeremy.hiatt@gmail.com>
2016-02-10 12:19:22 -0800

    Thrift service definition for message bus peer protocol

--- message_bus.thrift
+++ message_bus.thrift
@@ -29,3 +29,32 @@ struct SubscriptionNotification {
 struct Devices {
   1: list<Device> devices
 }
+
+service MessageBusPeerService {
+
+  Devices get_all()
+  Device get_device(1: string device_name)
+  Peripheral get_peripheral(1: string device_name, 2: string peripheral_name)
+
+  void set_variable(
+      1: string device_name,
+      2: string peripheral_name,
+      3: string variable_name,
+      4: string value,
+  )
+  void set_variable_request(
+      1: string device_name,
+      2: string peripheral_name,
+      3: string variable_name,
+      4: string value,
+  )
+  void set_device(1: Device device)
+
+  void subscribe_all(1: string subscriber_name)
+  void subscribe_device(1: string subscriber_name, 2: string device_name)
+  void subscribe_peripheral(
+      1: string subscriber_name,
+      2: string device_name,
+      3: string peripheral_name,
+  )
+}
