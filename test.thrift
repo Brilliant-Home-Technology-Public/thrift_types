cf136e30a6c4e826c2ac9f2046ad24ca811ad9c8
Jeremy Hiatt <jeremy.hiatt@gmail.com>
2016-02-10 12:04:38 -0800

    Library to inspect Thrift definitions and generate protocol classes

---
+++ test.thrift
@@ -0,0 +1,13 @@
+/* This really should be somewhere else */
+
+struct SimpleStruct {
+  1: string field1
+}
+
+service TestService {
+  void method_with_no_args()
+  void method_with_primitive_args(1: string arg1, 2: i32 arg2)
+  void method_with_struct_arg(1: SimpleStruct arg1)
+  i32 method_returning_primitive()
+  SimpleStruct method_returning_struct()
+}
