6d8f9c59c9c714c79ab6af7c41601681974d10c4
Cassie Rommel <cassandra.rommel@gmail.com>
2016-02-22 19:09:42 -0800

    sonos peripheral stuff

---
+++ music.thrift
@@ -0,0 +1,21 @@
+struct MusicTrack {
+  2: string title
+  3: string artist
+  4: string album
+  5: optional string album_art
+  6: optional i64 duration
+  7: i64 queue_position
+}
+
+struct PlayQueue {
+  1: list<MusicTrack> tracks
+}
+
+struct MusicPlayer {
+  1: string ip_address
+  2: string zone_name
+}
+
+struct MusicPlayers {
+  1: list<MusicPlayer> players
+}
