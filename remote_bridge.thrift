7cb31864737d1033a751b97b7c0bf881836bf474
Jeremy Hiatt <jeremy@brilliant.tech>
2016-02-26 14:03:59 -0800

    Initial implementation of remote bridge

---
+++ remote_bridge.thrift
@@ -0,0 +1,16 @@
+namespace py thrift_types.remote_bridge
+
+include "message_bus.thrift"
+
+service RemoteBridgeService {
+  message_bus.SetVariableResponse forward_set_variable_request(
+      1: string device_name,
+      2: string peripheral_name,
+      3: string variable_name,
+      4: string value,
+  )
+
+  void forward_notification(
+      1: message_bus.SubscriptionNotification notification,
+  )
+}
