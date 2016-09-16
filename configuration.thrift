6cceaa36d63282a9f78e7156f6d37409210487e3
ashley <ashchou3@gmail.com>
2016-09-16 12:55:42 -0700

    Modify art library to separate id from title

--- configuration.thrift
+++ configuration.thrift
@@ -7,12 +7,13 @@ struct ArtPiece {
 }
 
 struct ArtLibrary {
-  1: string name
-  2: list<ArtPiece> art_pieces
+  1: string id
+  2: string title
+  3: list<ArtPiece> art_pieces
 }
 
 struct ArtLibraryConfig {
-  1: string library_name
+  1: string library_id
   2: bool enabled
   3: list<string> disabled_art_pieces
 }
