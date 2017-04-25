009f0193e7848567611bc1c58f03bee12e9430a7
Cassandra Rommel <cassandra.rommel@gmail.com>
2017-04-25 14:15:52 -0700

    adding thirdparty discovery ttypes

--- discovery.thrift
+++ discovery.thrift
@@ -9,3 +9,20 @@ struct DeviceInterface {
 struct DeviceInterfaces {
   1: list<DeviceInterface> device_interfaces
 }
+
+enum ThirdpartyDeviceType {
+	UNKNOWN = 0,
+	SONOS = 1,
+	PHILIPS_HUE = 2,
+}
+
+struct ThirdpartyDeviceInterface {
+  1: string ip_address
+  2: i32 port
+  3: ThirdpartyDeviceType device_type
+}
+
+struct ThirdpartyDeviceInterfaces {
+	1: list<ThirdpartyDeviceInterface> device_interfaces
+}
+
