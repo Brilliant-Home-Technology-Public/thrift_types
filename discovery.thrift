e2ed7c4b71f6aadf7e90968c759b9875fe4414a5
Ananda Bose <ananda@brilliant.tech>
2017-06-15 11:20:21 -0700

    Add thrift constant for Wemo switch to use during discovery

--- discovery.thrift
+++ discovery.thrift
@@ -3,7 +3,9 @@ namespace py thrift_types.discovery
 const string WEMO_DIMMER_MODEL_NAME = "Dimmer"
 const string WEMO_INSIGHT_MODEL_NAME = "Insight"
 const string WEMO_MINI_MODEL_NAME = "Socket"
-const string WEMO_SWITCH_MODEL_NAME = "Lightswitch"
+// The Wemo Switch device name has different casing between discovery and their XML API :(
+const string WEMO_SWITCH_MODEL_NAME = "LightSwitch"
+const string WEMO_SWITCH_MODEL_NAME_FOR_DISCOVERY = "Lightswitch"
 
 struct DeviceInterface {
   1: string device_id
