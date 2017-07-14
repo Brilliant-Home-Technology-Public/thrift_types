999012caabe93dfe9f22772daf768539ee09240e
Ananda Bose <ananda@brilliant.tech>
2017-07-13 17:45:28 -0700

    Add thrift interface for analytics peripheral

---
+++ peripheral_interfaces/analytics_interface.thrift
@@ -0,0 +1,14 @@
+namespace py thrift_types.peripheral_interfaces.analytics_interface
+
+include "../message_bus.thrift"
+include "peripheral_interface.thrift"
+
+struct AnalyticsPeripheralInterface {
+  1: string socket_path
+}
+
+const peripheral_interface.PeripheralInterface peripheral = {
+  'peripheral_interface_name': 'AnalyticsPeripheralInterface',
+  'peripheral_type': message_bus.PeripheralType.ANALYTICS
+}
+
