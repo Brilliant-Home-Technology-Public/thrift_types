ebc1b43da12364dcf68532b6adc893a87ba71a42
Ananda Bose <ananda@brilliant.tech>
2017-07-27 16:37:25 -0700

    add thrift interface for thermostats

--- peripheral_interfaces/thermostat_interface.thrift
+++ peripheral_interfaces/thermostat_interface.thrift
@@ -6,8 +6,8 @@ include "peripheral_interface.thrift"
 struct ThermostatPeripheralInterface {
   1: i32 target_temperature_f
   2: i32 ambient_temperature_f
-  4: i32 min_temperature_f
-  5: i32 max_temperature_f
+  3: i32 min_temperature_f
+  4: i32 max_temperature_f
 }
 
 const peripheral_interface.PeripheralInterface peripheral = {
