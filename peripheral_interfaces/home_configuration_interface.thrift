7b29d01d4e66239e774264b0abd40f09b810e4d7
Ananda Bose <ananda@brilliant.tech>
2017-09-12 17:11:05 -0700

    add interface definition for home configuraiton peripheral

---
+++ peripheral_interfaces/home_configuration_interface.thrift
@@ -0,0 +1,17 @@
+namespace py thrift_types.peripheral_interfaces.home_configuration_interface
+
+include "../configuration.thrift"
+include "../message_bus.thrift"
+include "peripheral_interface.thrift"
+
+struct HomeConfigurationPeripheralInterface {
+  1: string name
+  2: configuration.Rooms rooms
+  3: string thridparty_discovery_owner
+  4: string local_timezone
+}
+
+const peripheral_interface.PeripheralInterface peripheral = {
+  'peripheral_interface_name': 'HomeConfigurationPeripheralInterface',
+  'peripheral_type': message_bus.PeripheralType.HOME_CONFIGURATION
+}
