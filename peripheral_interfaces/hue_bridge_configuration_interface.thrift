9d637c5d759f1f33ed3bca7400400cd2c67eaf87
Ananda Bose <ananda@brilliant.tech>
2017-10-03 15:45:16 -0700

    add peripheral interface for Hue bridge

---
+++ peripheral_interfaces/hue_bridge_configuration_interface.thrift
@@ -0,0 +1,14 @@
+namespace py thrift_types.peripheral_interfaces.hue_bridge_configuration_interface
+
+include "../configuration.thrift"
+include "../message_bus.thrift"
+include "peripheral_interface.thrift"
+
+struct HueBridgeConfigurationPeripheralInterface {
+  1: configuration.HueBridgeCredentials credentials
+}
+
+const peripheral_interface.PeripheralInterface peripheral = {
+  'peripheral_interface_name': 'HueBridgeConfigurationPeripheralInterface',
+  'peripheral_type': message_bus.PeripheralType.HUE_BRIDGE_CONFIGURATION,
+}
