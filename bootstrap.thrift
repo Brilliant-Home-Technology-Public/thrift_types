1b82508a8bbb42728a20d4e46724bbb55e148fef
Jeremy Hiatt <jeremy@brilliant.tech>
2017-10-02 21:34:41 -0700

    bootstrap: Update values for status enum

--- bootstrap.thrift
+++ bootstrap.thrift
@@ -3,7 +3,10 @@ namespace py thrift_types.bootstrap
 enum BootstrapStatus {
   IDLE = 0,
   SCANNING = 1,
-  AUTHENTICATING = 2,
-  COMPLETED = 3,
-  FAILED = 4,
+  KNOCKING = 2,
+  HOME_BUSY = 3,
+  HOME_THROTTLED = 4,
+  AUTHENTICATING = 5,
+  COMPLETED = 6,
+  AUTHENTICATION_FAILED = 7,
 }
