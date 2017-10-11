10e475da18fa8b24928c8304c18ee13d26dbc912
Jeremy Hiatt <jeremy@brilliant.tech>
2017-10-10 17:44:07 -0700

    peripheral_interfaces: discovery_interface: Set proper type for available_services

--- peripheral_interfaces/discovery_interface.thrift
+++ peripheral_interfaces/discovery_interface.thrift
@@ -5,7 +5,7 @@ include "../message_bus.thrift"
 include "peripheral_interface.thrift"
 
 struct DiscoveryPeripheralInterface {
-  1: string available_services
+  1: discovery.DeviceInterfaces available_services
   2: discovery.ThirdpartyDeviceInterfaces available_upnp_services
   3: bool discovery_mode_on
   4: bool discovery_mode_available
