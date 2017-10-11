f1bc986643fca3dec50987403f1fe0cc8d9f2bec
Jeremy Hiatt <jeremy@brilliant.tech>
2017-10-11 11:38:37 -0700

    peripheral_interfaces: bootstrap_interface: Initial version

---
+++ peripheral_interfaces/bootstrap_interface.thrift
@@ -0,0 +1,18 @@
+namespace py thrift_types.peripheral_interfaces.bootstrap_interface
+
+include "../bootstrap.thrift"
+include "../message_bus.thrift"
+include "peripheral_interface.thrift"
+
+struct BootstrapPeripheralInterface {
+  1: bootstrap.AvailableHomes available_homes
+  2: i32 authentication_code
+  3: bootstrap.BootstrapStatus status
+  4: string target_home_id
+  5: string server_authentication_token
+}
+
+const peripheral_interface.PeripheralInterface peripheral = {
+  'peripheral_interface_name': 'BootstrapPeripheralInterface',
+  'peripheral_type': message_bus.PeripheralType.BOOTSTRAP
+}
