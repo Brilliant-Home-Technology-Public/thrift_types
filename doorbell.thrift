648e829353bd23f3d8baabebd48978bad7d38504
Cassandra Rommel <cassandra.rommel@gmail.com>
2018-02-07 16:45:22 -0800

    Add doorbell constants to new doorbell.thrift
    CR: Ananda

---
+++ doorbell.thrift
@@ -0,0 +1,8 @@
+namespace py thrift_types.doorbell
+namespace java thrift_types.doorbell
+
+// Ring Pro model names currently are: lpd_v1 and lpd_v2
+const string RING_PRO_MODEL_PREFIX = "lpd"
+// Ring Elite model names currently are: jbox_v1
+const string RING_ELITE_MODEL_PREFIX = "jbox"
+
