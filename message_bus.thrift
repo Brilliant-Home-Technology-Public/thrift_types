a15cfca97ba9c4c99b01496629649a3fcbe83699
Ryan Rueth <ryan@brilliant.tech>
2018-04-25 14:53:29 -0700

    Set the Device.timestamp to zero by default
    CR: Jeremy

--- message_bus.thrift
+++ message_bus.thrift
@@ -167,9 +167,7 @@ struct Peripheral {
 struct Device {
   1: string id
   2: map<string, Peripheral> peripherals
-  // Make the timestamp optional until we can ensure that the timestamp is set in all uses of the
-  // Device struct. Otherwise, the mobile app will explode in the meantime.
-  3: optional i64 timestamp
+  3: i64 timestamp = 0
 }
 
 struct VirtualDeviceRegistration {
