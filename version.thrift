5aad2691bca2a90b88517005f542ab8d78074c9e
Kiyoshi Shikuma <kiyoshi@brilliant.tech>
2018-05-01 14:32:08 -0700

    Add a constant for the current api version
    CR: Ryan

--- version.thrift
+++ version.thrift
@@ -1,5 +1,10 @@
 namespace py thrift_types.version
 namespace java thrift_types.version
 
+// MOST RECENT VERSION:
 const string VERSION_20180420 = "20180420"
+const string CURRENT_API_VERSION = VERSION_20180420
+
+// OLD VERSIONS:
 const string VERSION_20180221 = "20180221"
+
