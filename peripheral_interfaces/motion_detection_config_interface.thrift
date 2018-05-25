70f5e0dce064ceba59a102bb9c1c92414bdedcc6
Ashley Chou <ashchou3@gmail.com>
2018-05-25 14:52:44 -0700

    Create MotionDetectionConfigPeripheralInterface
    CR: Jeremy

---
+++ peripheral_interfaces/motion_detection_config_interface.thrift
@@ -0,0 +1,18 @@
+namespace py thrift_types.peripheral_interfaces.motion_detection_config_interface
+namespace java thrift_types.peripheral_interfaces.motion_detection_config_interface
+
+include "../configuration.thrift"
+include "../message_bus.thrift"
+include "peripheral_interface.thrift"
+
+struct MotionDetectionConfigPeripheralInterface {
+  1: bool trigger_screen
+  2: i32 trigger_screen_off_timeout_sec
+}
+
+const peripheral_interface.PeripheralInterface peripheral = {
+  'peripheral_interface_name': 'MotionDetectionConfigPeripheralInterface',
+  'peripheral_type': message_bus.PeripheralType.MOTION_DETECTION_CONFIGURATION,
+  'dynamic_variable_prefix': configuration.LIGHT_MOTION_CONFIG_VARIABLE_PREFIX,
+  'dynamic_variable_ttype': 'configuration.LightMotionConfig',
+}
