2797a076736aa6deafda4b8439584b9c9bc8a3f4
Ashley Chou <ashchou3@gmail.com>
2018-06-04 18:48:45 -0700

    Add 20180605 version and set as current
    CR: Ashu

--- version.thrift
+++ version.thrift
@@ -2,8 +2,9 @@ namespace py thrift_types.version
 namespace java thrift_types.version
 
 // MOST RECENT VERSION:
+const string VERSION_20180605 = "20180605"
 const string VERSION_20180420 = "20180420"
-const string CURRENT_API_VERSION = VERSION_20180420
+const string CURRENT_REMOTE_BRIDGE_API_VERSION = VERSION_20180420
 
 // OLD VERSIONS:
 const string VERSION_20180221 = "20180221"
