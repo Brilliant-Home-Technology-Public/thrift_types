e7eab62e0a1516c1e3dc33493555d74bc948e4d7
Jeremy Hiatt <jeremy@brilliant.tech>
2018-06-20 15:18:03 -0700

    version: Define new version
    CR: Ashley

--- version.thrift
+++ version.thrift
@@ -2,10 +2,10 @@ namespace py thrift_types.version
 namespace java thrift_types.version
 
 // MOST RECENT VERSION:
-const string VERSION_20180605 = "20180605"
-const string VERSION_20180420 = "20180420"
-const string CURRENT_REMOTE_BRIDGE_API_VERSION = VERSION_20180420
+const string VERSION_20180620 = "20180620"
+const string CURRENT_REMOTE_BRIDGE_API_VERSION = VERSION_20180620
 
 // OLD VERSIONS:
+const string VERSION_20180605 = "20180605"
+const string VERSION_20180420 = "20180420"
 const string VERSION_20180221 = "20180221"
-
