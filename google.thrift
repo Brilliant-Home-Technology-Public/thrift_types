b798533b30f8733fbe165c35be68d1e40d2a9491
Cassandra Rommel <cassandra.rommel@gmail.com>
2018-06-26 19:55:29 -0700

    Add google thrift file
    CR: Ananda

---
+++ google.thrift
@@ -0,0 +1,6 @@
+namespace py thrift_types.google
+namespace java thrift_types.google
+
+# Provider identifier is used to store the global google jwt token 
+const string GOOGLE_PROVIDER_IDENTIFIER = "google"
+
