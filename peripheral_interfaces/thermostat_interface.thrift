d47dacb12eb230376fe56e3865d8cae9142a3606
Aman Dhar <aman@brilliant.tech>
2018-07-06 16:42:24 -0700

    fixup! Added temperature_ranges and target temp high and low variables to thermostats to help deal with auto-modes with target temperature ranges.

--- peripheral_interfaces/thermostat_interface.thrift
+++ peripheral_interfaces/thermostat_interface.thrift
@@ -6,13 +6,13 @@ include "../thermostat.thrift"
 include "peripheral_interface.thrift"
 
 struct ThermostatPeripheralInterface {
+  // both target_temperature_low_f and target_temperature_high_f should be
+  // used mutually exclusively with target_temperature_f (depending on hvac_mode)
   1: optional i32 target_temperature_f
   2: i32 ambient_temperature_f
   3: thermostat.ThermostatCapabilities thermostat_capabilities
   4: thermostat.HVACMode hvac_mode
   5: optional thermostat.FanMode fan_mode // Some heaters won't have a fan
-  // both target_temperature_low_f and target_temperature_high_f should be
-  // used mutually exclusively with target_temperature_f (depending on hvac_mode)
   6: optional i32 target_temperature_low_f
   7: optional i32 target_temperature_high_f
   8: thermostat.TemperatureRanges temperature_ranges
