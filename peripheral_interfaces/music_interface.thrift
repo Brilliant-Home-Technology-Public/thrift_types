7b4db4b66b277b5c527bc081e463426941990e86
Ananda Bose <ananda@brilliant.tech>
2018-07-09 11:37:15 -0700

    Replace MusicPeripheralInterface with MusicPeripheralInterfaceV2 and rename
    CR: Cassie

--- peripheral_interfaces/music_interface.thrift
+++ peripheral_interfaces/music_interface.thrift
@@ -7,12 +7,15 @@ include "peripheral_interface.thrift"
 struct MusicPeripheralInterface {
   1: bool playing
   2: music.MusicTrack current_track_info
-  3: music.PlayQueue play_queue
-  4: i32 queue_position
-  5: i32 volume
-  6: music.Playlists playlists
-  7: music.Playlists favorites
-  8: i32 position
+  3: bool can_skip       // can skip forward from current track
+  4: bool skip           // skip forward from current track
+  5: bool can_skip_back  // can skip backward from current track
+  6: bool skip_back      // skip backward from current track
+  7: i32 track_position  // position within current track
+  8: i32 volume
+  9: bool volume_fixed   // if volume can be adjusted or not
+  10: string play_uri    // initiate playback of a favorite
+  11: music.Playlists favorites
 }
 
 const peripheral_interface.PeripheralInterface peripheral = {
