1b66c8f379abec0935d1bb17c886c58bde691de1
Jeremy Hiatt <jeremy@brilliant.tech>
2018-07-23 12:40:45 -0700

    message_bus: Add deleted boolean to SubscriptionNotification
    CR: Ashley

--- message_bus.thrift
+++ message_bus.thrift
@@ -207,6 +207,7 @@ struct SubscriptionNotification {
   1: Device updated_device
   2: i64 timestamp
   3: optional list<ModifiedPeripheral> modified_peripherals = []
+  4: bool deleted = false
 }
 
 struct Devices {
