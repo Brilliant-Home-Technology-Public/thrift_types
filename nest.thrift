5ac58b96c22ffbac3c9b416ced450a5a5c5dc687
Patrick Mogan <patrick@brilliant.tech>
2018-08-29 13:45:59 -0700

    nest.thrift: Add nest demo access token string constant
    CR: Cassie

--- nest.thrift
+++ nest.thrift
@@ -4,6 +4,7 @@ namespace java thrift_types.nest
 const string NEST_API_BASE = "https://developer-api.nest.com"
 // Required min difference between heat and cool temps in heat-cool mode
 const i32 HEAT_COOL_MIN_DELTA = 3
+const string NEST_DEMO_ACCESS_TOKEN = "NEST_DEMO_ACCESS_TOKEN"
 
 struct ThermostatIds {
 	1: list<string> thermostat_ids
