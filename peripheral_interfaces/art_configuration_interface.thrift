662279fbf4439de96179495d4a3e8db328db0a57
Ryan Rueth <ryan@brilliant.tech>
2018-09-19 19:12:58 -0700

    Add RoomAssignment to art_configuration_interface.thrift
    CR: Ananda

--- peripheral_interfaces/art_configuration_interface.thrift
+++ peripheral_interfaces/art_configuration_interface.thrift
@@ -1,6 +1,7 @@
 namespace py thrift_types.peripheral_interfaces.art_configuration_interface
 namespace java thrift_types.peripheral_interfaces.art_configuration_interface
 
+include "../configuration.thrift"
 include "../message_bus.thrift"
 include "peripheral_interface.thrift"
 
@@ -8,6 +9,7 @@ struct ArtConfigurationPeripheralInterface {
   1: bool display_time_date
   2: i32 display_frequency
   3: bool on
+  4: configuration.RoomAssignment room_assignment
 }
 
 const peripheral_interface.PeripheralInterface peripheral = {
