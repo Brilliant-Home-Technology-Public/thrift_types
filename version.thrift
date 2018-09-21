68e0ce55145738d9ec0e135031ece2be66d11f4c
Ted Ford <ted@brilliant.tech>
2018-09-21 11:24:25 -0700

    Update Thrift Types
    Add VERSION_20180925 as the current version

--- version.thrift
+++ version.thrift
@@ -2,9 +2,10 @@ namespace py thrift_types.version
 namespace java thrift_types.version
 
 // MOST RECENT VERSION:
-const string VERSION_20180808 = "20180808"
+const string VERSION_20180925 = "20180925"
 
 // OLD VERSIONS:
+const string VERSION_20180808 = "20180808"
 const string VERSION_20180620 = "20180620"
 const string VERSION_20180605 = "20180605"
 const string VERSION_20180420 = "20180420"
