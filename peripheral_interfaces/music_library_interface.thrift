f39c992ee0c83c1c8ca75331f6d97a0deb311ab2
Ananda Bose <ananda@brilliant.tech>
2018-09-21 17:21:36 -0700

    Add peripheral interface for MUSIC_LIBRARY peripherals.
    CR: Alvin

---
+++ peripheral_interfaces/music_library_interface.thrift
@@ -0,0 +1,15 @@
+namespace py thrift_types.peripheral_interfaces.music_library_interface
+namespace java thrift_types.peripheral_interfaces.music_library_interface
+
+include "../message_bus.thrift"
+include "../music.thrift"
+include "peripheral_interface.thrift"
+
+struct MusicLibraryPeripheralInterface {
+  1: music.Playlists favorites
+}
+
+const peripheral_interface.PeripheralInterface peripheral = {
+  'peripheral_interface_name': 'MusicLibraryPeripheralInterface',
+  'peripheral_type': message_bus.PeripheralType.MUSIC_LIBRARY
+}
