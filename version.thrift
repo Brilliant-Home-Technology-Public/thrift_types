68e69de0bc7deba89b0232e877b57e91fce4bb32
Ashley Chou <ashchou3@gmail.com>
2018-10-16 18:55:15 -0700

    Add VERSION_20181018
    CR: Ashu

--- version.thrift
+++ version.thrift
@@ -2,9 +2,10 @@ namespace py thrift_types.version
 namespace java thrift_types.version
 
 // MOST RECENT VERSION:
-const string VERSION_20181005 = "20181005"
+const string VERSION_20181018 = "20181018"
 
 // OLD VERSIONS:
+const string VERSION_20181005 = "20181005"
 const string VERSION_20180925 = "20180925"
 const string VERSION_20180808 = "20180808"
 const string VERSION_20180620 = "20180620"
