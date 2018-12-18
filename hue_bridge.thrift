1a62e4a527ffa30a8dfe51448eb220f92aa21230
Dalton Petursson <dalton@brilliant.tech>
2018-12-17 20:06:02 -0800

    Adds consts for Hue device types

--- hue_bridge.thrift
+++ hue_bridge.thrift
@@ -6,7 +6,9 @@ namespace java thrift_types.hue_bridge
 //   - _API_NAME consts also serve as HueBridgePeripheral types.
 //       this info is stored in the generated peripheral IDs (see hue_bridge_utils.py)
 
+const string HUE_LIGHT_TYPE_NAME = "light"
 const string HUE_LIGHT_API_NAME = "lights"
 const string HUE_LIGHT_API_ENDPOINT_UPDATE = "state"
+const string HUE_GROUP_TYPE_NAME = "group"
 const string HUE_GROUP_API_NAME = "groups"
 const string HUE_GROUP_API_ENDPOINT_UPDATE = "action"
