db6c494dc89c447aac7377a0f462559d371ee83a
Ted Ford <ted@brilliant.tech>
2019-01-28 10:31:34 -0800

    Add Version 20190130
    CR: Alvin

--- version.thrift
+++ version.thrift
@@ -2,9 +2,10 @@ namespace py thrift_types.version
 namespace java thrift_types.version
 
 // MOST RECENT VERSION:
-const string VERSION_20181018 = "20181018"
+const string VERSION_20190130 = "20190130"
 
 // OLD VERSIONS:
+const string VERSION_20181018 = "20181018"
 const string VERSION_20181005 = "20181005"
 const string VERSION_20180925 = "20180925"
 const string VERSION_20180808 = "20180808"
