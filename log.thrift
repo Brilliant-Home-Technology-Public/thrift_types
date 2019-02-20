eba2c2140338d42e00ec9a5163a83da44816a018
Ted Ford <ted@brilliant.tech>
2019-02-19 18:03:54 -0800

    Add Button Name Logging Constant UI_BUTTON_NAME_CONFIRM_OK
    Add a constant for logging the "OK" button.
    
    CR: Alvin

--- log.thrift
+++ log.thrift
@@ -272,6 +272,7 @@ const string UI_BUTTON_NAME_CONFIRM_DELETE_ALARM = "confirm:delete_alarm"
 const string UI_BUTTON_NAME_DISMISS = "dismiss"
 const string UI_BUTTON_NAME_DELETE_CONFIRM = "delete_confirm"
 const string UI_BUTTON_NAME_RESET_CONFIRM = "reset_confirm"
+const string UI_BUTTON_NAME_CONFIRM_OK = "confirm:ok"
 
 // NOTE:DO NOT ADD ANY FIELDS TO SimpleButtonTappedEvent STRUCT
 // If A ButtonTapped log needs another field, make a new struct
