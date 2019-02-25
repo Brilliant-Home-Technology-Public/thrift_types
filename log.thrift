06aef818ceafda57a0070176c57225389233b291
alvin-yuan <alvin@brilliant.tech>
2019-02-25 13:18:37 -0800

    Add button log constants for hidden and discovered network
    CR: Ashley

--- log.thrift
+++ log.thrift
@@ -274,6 +274,8 @@ const string UI_BUTTON_NAME_DELETE_CONFIRM = "delete_confirm"
 const string UI_BUTTON_NAME_RESET_CONFIRM = "reset_confirm"
 const string UI_BUTTON_NAME_CONFIRM_OK = "confirm:ok"
 const string UI_BUTTON_NAME_FORGET_NETWORK = "forget_network"
+const string UI_BUTTON_NAME_HIDDEN_NETWORK = "hidden_network"
+const string UI_BUTTON_NAME_DISCOVERED_NETWORK = "discovered_network"
 
 // NOTE:DO NOT ADD ANY FIELDS TO SimpleButtonTappedEvent STRUCT
 // If A ButtonTapped log needs another field, make a new struct
