8b90e121a6248e0282beecb3fd744b2d98e795ec
Ted Ford <ted@brilliant.tech>
2019-04-08 12:24:18 -0700

    Add SceneValidityStates to Scene Configuration
    CR: Ashley

--- peripheral_interfaces/scene_interface.thrift
+++ peripheral_interfaces/scene_interface.thrift
@@ -1,11 +1,13 @@
 namespace py thrift_types.peripheral_interfaces.scene_interface
 namespace java thrift_types.peripheral_interfaces.scene_interface
 
+include "../configuration.thrift"
 include "../message_bus.thrift"
 include "peripheral_interface.thrift"
 
 struct ScenePeripheralInterface {
   1: string executing_device_id
+  2: optional configuration.SceneValidityStates scene_validity_states
 }
 
 const peripheral_interface.PeripheralInterface peripheral = {
