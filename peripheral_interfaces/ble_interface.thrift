c45e53356a4ec9d02edd36e52ca39bf798e5d5e3
Bo <bo@brilliant.tech>
2019-06-07 15:24:57 -0700

    Adding new peripheral interfaces
      - ble interface for peripheral that would translate actions
        to calls to ble adapters / proxy mesh client
      - mesh configuration interface to represent mesh configurations
        within the configuration virtual device
    
      CR: James

---
+++ peripheral_interfaces/ble_interface.thrift
@@ -0,0 +1,17 @@
+namespace py thrift_types.peripheral_interfaces.ble_interface
+namespace java thrift_types.peripheral_interfaces.ble_interface
+
+include "../bluetooth.thrift"
+include "../message_bus.thrift"
+include "peripheral_interface.thrift"
+
+struct BlePeripheralInterface {
+  1: i16 unicast_address
+  2: bluetooth.SeqBlock seq_block
+  3: bluetooth.ReplayProtection replay_protection
+}
+
+const peripheral_interface.PeripheralInterface ble_peripheral = {
+  'peripheral_interface_name': 'BlePeripheralInterface',
+  'peripheral_type': message_bus.PeripheralType.BLE,
+}
---
+++ peripheral_interfaces/mesh_configuration_interface.thrift
@@ -0,0 +1,23 @@
+namespace py thrift_types.peripheral_interfaces.mesh_configuration_interface
+namespace java thrift_types.peripheral_interfaces.mesh_configuration_interface
+
+include "../bluetooth.thrift"
+include "../message_bus.thrift"
+include "peripheral_interface.thrift"
+
+struct MeshConfigurationPeripheralInterface {
+  1: bluetooth.AppKeys app_keys
+  2: bluetooth.NetKeys net_keys
+  3: i32 iv_index
+  4: i64 iv_index_update_mode_trigger_time_ms
+  5: i64 iv_index_recovery_time_ms
+  6: string relay_arbiter_device_id
+  7: i32 next_unicast_address
+}
+
+const peripheral_interface.PeripheralInterface peripheral = {
+  'peripheral_interface_name': 'MeshConfigurationPeripheralInterface',
+  'peripheral_type': message_bus.PeripheralType.MESH_CONFIGURATION,
+  'dynamic_variable_prefix': bluetooth.MESH_CONFIG_VARIABLE_PREFIX,
+  'dynamic_variable_ttype': 'bluetooth.MeshDeviceConfig',
+}
