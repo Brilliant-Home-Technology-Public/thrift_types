37011c855603509b72164557ef62a5fdf75b9c15
Aman Dhar <aman@brilliant.tech>
2019-06-19 17:33:20 -0700

    Revert "Revert "Added prefix to separate sonos playlist URIs and favorite URIs.""
    This reverts commit 4fc9916f887a557bb0a4870b19a5cb002b85104c.
    
    CR: Ananda

--- sonos.thrift
+++ sonos.thrift
@@ -1,6 +1,9 @@
 namespace py thrift_types.sonos
 namespace java thrift_types.sonos
 
+// For favorites, set uri to "<raw_uri>". For playlists, set uri to "playlist:<raw_uri>"
+const string PLAYLIST_URI_PREFIX = "playlist"
+
 enum GroupStatus {
   UNKNOWN = 0,
   MOVED = 1,
