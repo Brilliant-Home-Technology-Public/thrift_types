06a4aec8da327c40b94a49812b4c316788db1172
Aman Dhar <aman@brilliant.tech>
2019-08-09 14:07:22 -0700

    Adding the subscription key request header key to schlage thrift. - Requests to schlage api should have <this_constant>: <actual_subscription_key> in the request header
    CR: Julian

--- schlage.thrift
+++ schlage.thrift
@@ -2,3 +2,4 @@ namespace py thrift_types.schlage
 namespace java thrift_types.schlage
 
 const string SCHLAGE_API_PREFIX = "https://api.allegion.com/schlage-home/api/"
+const string SUBSCRIPTION_KEY_REQUEST_HEADER_KEY = "alle-subscription-key"
