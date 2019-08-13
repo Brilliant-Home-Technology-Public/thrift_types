0e4e5c96682b1d1dddedee064927a05d7205d260
James Huang <hng.jms@gmail.com>
2019-08-13 16:01:06 -0700

    Add Proxy Service UUID
    CR: bo

--- bluetooth.thrift
+++ bluetooth.thrift
@@ -2,6 +2,7 @@ namespace py thrift_types.bluetooth
 namespace java thrift_types.bluetooth
 
 
+const string MESH_PROXY_SERVICE_UUID_STR = "00001828-0000-1000-8000-00805f9b34fb"
 const string MESH_PROV_DATA_IN_UUID_STR = "00002adb-0000-1000-8000-00805f9b34fb"
 const string MESH_PROV_DATA_OUT_UUID_STR = "00002adc-0000-1000-8000-00805f9b34fb"
 const string MESH_PROXY_DATA_IN_UUID_STR ="00002add-0000-1000-8000-00805f9b34fb"
