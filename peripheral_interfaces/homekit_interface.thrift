a3d3e505e92158b102307dfe1e2157b968e95b1a
Ashley Chou <ashchou3@gmail.com>
2019-08-23 20:00:51 -0700

    [peripheral_interfaces] Define HomeKitPeripheralInterface
    CR: Jeremy / Alvin

---
+++ peripheral_interfaces/homekit_interface.thrift
@@ -0,0 +1,18 @@
+namespace py thrift_types.peripheral_interfaces.homekit_interface
+namespace java thrift_types.peripheral_interfaces.homekit_interface
+
+include "../message_bus.thrift"
+include "peripheral_interface.thrift"
+
+struct HomeKitPeripheralInterface {
+  1: bool enabled
+  2: string setup_uri
+  3: string setup_id
+  4: string setup_code
+  5: bool reset
+}
+
+const peripheral_interface.PeripheralInterface peripheral = {
+  'peripheral_interface_name': 'HomeKitPeripheralInterface',
+  'peripheral_type': message_bus.PeripheralType.HOMEKIT,
+}
