fd8fc3b184f8ef822532cd1d55b75511c0769caa
Ananda Bose <ananda@brilliant.tech>
2019-09-07 20:07:28 -0700

    add Thrift constants for ButterflyMX APIs

---
+++ butterflymx.thrift
@@ -0,0 +1,6 @@
+namespace py thrift_types.butterflymx
+namespace java thrift_types.butterflymx
+
+const string BUTTERFLYMX_API_URL_BASE = "https://apitest.butterflymx.com"
+const string BUTTERFLYMX_ACCOUNT_URL_BASE = "https://accountstest.butterflymx.com"
+const string BUTTERFLYMX_PERIPHERAL_ID = "butterflymx_panel_peripheral"
