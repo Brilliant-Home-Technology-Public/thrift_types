422026ebb2f18a5aca0c0ca42be75a7f0fb51db2
Jeremy Hiatt <jeremy@brilliant.tech>
2019-10-25 13:44:18 -0700

    Add MediaTransmission enum

--- butterflymx.thrift
+++ butterflymx.thrift
@@ -4,3 +4,11 @@ namespace java thrift_types.butterflymx
 const string BUTTERFLYMX_API_URL_BASE = "https://apitest.butterflymx.com"
 const string BUTTERFLYMX_ACCOUNT_URL_BASE = "https://accountstest.butterflymx.com"
 const string BUTTERFLYMX_PERIPHERAL_ID = "butterflymx_panel_peripheral"
+
+enum MediaTransmission {
+  NONE = 0
+  // Chosen so AUDIO_AND_VIDEO == (AUDIO | VIDEO)
+  AUDIO = 1
+  VIDEO = 2
+  AUDIO_AND_VIDEO = 3
+}
