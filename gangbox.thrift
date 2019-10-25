7c52a31bd57de376050e681c5b06a0646844c569
alvin-yuan <alvin@brilliant.tech>
2019-10-25 14:45:11 -0700

    Add DimSmoothingConfiguration
    CR: Ashley

---
+++ gangbox.thrift
@@ -0,0 +1,18 @@
+namespace py thrift_types.gangbox
+namespace java thrift_types.gangbox
+
+enum EasingType {
+  NO_EASING = 0,
+  LINEAR = 1,
+  EASE_OUT_CUBIC = 2,
+}
+
+struct DimSmoothingConfiguration {
+  1: EasingType easing_type
+  // Required for all easing types except for NO_EASING
+  2: optional i32 step_interval_ms
+  3: optional i32 min_step_magnitude
+  4: optional i32 max_fade_in_duration_ms
+  5: optional i32 max_fade_out_duration_ms
+  6: optional bool use_proportional_duration
+}
