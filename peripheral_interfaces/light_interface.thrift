2a3e61432cc362d14908d1dd4a680ff95f5adcd2
Mayuka B <mayuka@brilliant.tech>
2019-10-29 16:48:05 -0700

    Update light_interface to include color variables
    CR: Ananda

--- peripheral_interfaces/light_interface.thrift
+++ peripheral_interfaces/light_interface.thrift
@@ -2,12 +2,19 @@ namespace py thrift_types.peripheral_interfaces.light_interface
 namespace java thrift_types.peripheral_interfaces.light_interface
 
 include "../message_bus.thrift"
+include "../color.thrift"
 include "peripheral_interface.thrift"
 
 struct LightPeripheralInterface {
   1: bool on
   2: bool dimmable
   3: i32 intensity
+  4: optional bool supports_color
+  5: optional bool supports_temp
+  6: optional color.Color color
+  7: optional color.ColorList recently_saved_colors
+  8: optional i32 min_temperature
+  9: optional i32 max_temperature
 }
 
 const peripheral_interface.PeripheralInterface peripheral = {
