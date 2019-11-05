ebe7dbd88b7dbd8c4c117790c8612251c0f8e354
alvin-yuan <alvin@brilliant.tech>
2019-11-04 16:56:04 -0800

    Add UI_NOTIFICATION_OPTION_BUTTON_TAPPED_TABLE_NAME
    CR: Ted

--- log.thrift
+++ log.thrift
@@ -121,6 +121,7 @@ const string UI_IMPORT_PARTNER_SCENE_TABLE_NAME = "import_partner_scene"
 const string UI_SCENE_EXECUTION_TABLE_NAME = "ui_scene_execution"
 const string UI_HOMESCREEN_NOTIFICATION_VIEW_TABLE_NAME = "ui_homescreen_notification_view"
 const string UI_HOMESCREEN_NOTIFICATION_BUTTON_TAPPED_TABLE_NAME = "ui_homescreen_notification_button_tapped"
+const string UI_NOTIFICATION_OPTION_BUTTON_TAPPED_TABLE_NAME = "ui_notification_option_button_tapped"
 // END QT LOG TABLE NAMES
 
 // BEGIN EMBEDDED (MESSAGE BUS/PERIPHERALS) TABLE NAMES
