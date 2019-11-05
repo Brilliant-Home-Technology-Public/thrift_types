2da4fda58c32f40d73607f8aa76e5bf2eb15e96b
alvin-yuan <alvin@brilliant.tech>
2019-11-05 10:54:18 -0800

    Add gangbox light fields to LightPeripheralInterface as optional
    CR: Ashley

--- peripheral_interfaces/light_interface.thrift
+++ peripheral_interfaces/light_interface.thrift
@@ -1,6 +1,8 @@
 namespace py thrift_types.peripheral_interfaces.light_interface
 namespace java thrift_types.peripheral_interfaces.light_interface
 
+include "../configuration.thrift"
+include "../gangbox.thrift"
 include "../message_bus.thrift"
 include "peripheral_interface.thrift"
 
@@ -8,6 +10,27 @@ struct LightPeripheralInterface {
   1: bool on
   2: bool dimmable
   3: i32 intensity
+  4: optional i32 max_intensity_value
+  5: optional bool dimming_edge
+  6: optional i32 minimum_dim_level
+  7: optional i32 maximum_dim_level
+  8: optional bool has_load
+  9: optional i32 power_threshold
+  10: optional bool low_wattage
+  11: optional bool multi_way
+  12: optional gangbox.DimSmoothingConfiguration dim_smoothing_config
+  13: optional string display_name
+  14: optional double temperature
+  15: optional string voltage_references
+  16: optional bool is_safe
+  17: optional configuration.RoomAssignment room_assignment
+  18: optional string ulid
+  19: optional bool on_off_inverted
+  20: optional i32 current_sensed
+  21: optional bool break_circuit
+  22: optional i32 current_zero_cross
+  23: optional bool break_dimming
+  24: optional bool enable_dimmability_check
 }
 
 const peripheral_interface.PeripheralInterface peripheral = {
