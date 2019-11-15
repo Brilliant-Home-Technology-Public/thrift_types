322d23aa7bf3191ea212df89497b16ec69cca17b
Jeremy Hiatt <jeremy@brilliant.tech>
2019-11-14 19:18:42 -0800

    butterflymx: Define event keys + enumerate API `status` value
    CR: Ananda

--- butterflymx.thrift
+++ butterflymx.thrift
@@ -4,6 +4,16 @@ namespace java thrift_types.butterflymx
 const string BUTTERFLYMX_PANEL_ID_PREFIX = "panel"
 const string BUTTERFLYMX_BUILDING_ID_PREFIX = "building"
 
+// Event keys
+const string EVENT_KEY_CALL_GUID = "butterflymx:call_guid"
+const string EVENT_KEY_CALL_STATUS = "butterflymx:call_status"
+
+// Status enum values
+// See https://apidocs.butterflymx.com/core/v3/production/index.html#current-user-status
+const string CALL_STATUS_INITIALIZING = "initializing"
+const string CALL_STATUS_CANCELED = "canceled"
+const string CALL_STATUS_CONNECTING = "connecting_sip"
+
 struct BuildingEntryPanels {
   1: list<string> building_entry_panel_ids
 }
