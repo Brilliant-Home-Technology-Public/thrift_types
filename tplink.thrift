24205eb8c4d18927846bd24fc23238a154a07d68
Mayuka B <mayuka@brilliant.tech>
2019-11-25 16:01:54 -0800

    Add TP-Link icons

--- tplink.thrift
+++ tplink.thrift
@@ -2,3 +2,25 @@ namespace py thrift_types.tplink
 namespace java thrift_types.tplink
 
 const string TPLINK_API_PREFIX = "https://external-api.tplinkra.com/v1/"
+const list<string> model_specific_icons = [
+  "HS100",
+  "HS103",
+  "HS105",
+  "HS107",
+  "HS110",
+  "HS200",
+  "HS210",
+  "HS220",
+  "KL100",
+  "KL200",
+  "KL430",
+  "KP200",
+  "KP300",
+  "KP303",
+  "KP400",
+  "LB100",
+  "LB200"
+]
+const string default_light_icon = "KL100"
+const string default_switch_icon = "HS200"
+const string default_outlet_icon = "HS103"
