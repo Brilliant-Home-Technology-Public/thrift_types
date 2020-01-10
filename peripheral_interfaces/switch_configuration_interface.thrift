4eec523a9992a220fc3cb9c247b7700a17a179c1
Ashley Chou <ashchou3@gmail.com>
2020-01-09 21:53:57 -0800

    Create peripheral interface for SWITCH_CONFIGURATION type
    CR:

---
+++ peripheral_interfaces/switch_configuration_interface.thrift
@@ -0,0 +1,16 @@
+namespace py thrift_types.peripheral_interfaces.switch_configuration_interface
+namespace java thrift_types.peripheral_interfaces.switch_configuration_interface
+
+include "../configuration.thrift"
+include "../message_bus.thrift"
+include "peripheral_interface.thrift"
+
+struct SwitchConfigurationPeripheralInterface {
+  1: string display_name
+  2: configuration.RoomAssignment room_assignment
+}
+
+const peripheral_interface.PeripheralInterface peripheral = {
+  'peripheral_interface_name': 'SwitchConfigurationPeripheralInterface',
+  'peripheral_type': message_bus.PeripheralType.SWITCH_CONFIGURATION,
+}
