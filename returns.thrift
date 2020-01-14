aed1ed8f3a1c052facbb18f8f28cd50c3343df41
Ashu Goel <ashu@brilliant.tech>
2020-01-13 16:23:26 -0800

    [returns] add UNFULFILLED return type
    CR: Ryan

--- returns.thrift
+++ returns.thrift
@@ -4,8 +4,13 @@ namespace java thrift_types.returns
 
 enum ReturnType {
   UNKNOWN = 0,
+  // The order has been fulfilled and is within the refund period.
   REFUND = 1,
+  // The order has been fulfilled but is outside the refund period, or the user
+  // just wants an exchange.
   ADVANCED_EXCHANGE = 2,
+  // The order has not yet been dropped.
   CANCELLATION = 3,
+  // The order has been dropped, but not completely fulfilled.
+  UNFULFILLED = 4,
 }
-
