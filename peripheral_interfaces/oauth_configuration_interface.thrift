7a8ba49e812c76fccb3a6ca2f8df5a572d65bbeb
alvin-yuan <alvin@brilliant.tech>
2020-01-29 15:22:18 -0800

    Add variables to OAuthConfigurationPeripheralInterface
    CR: Ashu

--- peripheral_interfaces/oauth_configuration_interface.thrift
+++ peripheral_interfaces/oauth_configuration_interface.thrift
@@ -6,6 +6,24 @@ include "peripheral_interface.thrift"
 
 
 struct OAuthConfigurationPeripheralInterface {
+  1: string august_api_key
+  2: string august_client_id
+  3: string butterflymx_client_id
+  4: string butterflymx_environment_id
+  5: string genie_api_key
+  6: string genie_api_prefix
+  7: string genie_client_id
+  8: string nest_client_id
+  9: string ecobee_client_id
+  10: string honeywell_client_id
+  11: string lifx_client_id
+  12: string ring_client_id
+  13: string schlage_api_key
+  14: string schlage_client_id
+  15: string schlage_subscription_key
+  16: string smartthings_client_id
+  17: string tplink_api_key
+  18: string tplink_client_id
 }
 
 const peripheral_interface.PeripheralInterface peripheral = {
