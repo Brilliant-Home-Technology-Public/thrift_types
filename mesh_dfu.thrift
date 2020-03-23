ca06d5caf299f489bfc8fd5443e5170da72ab423
Bo See <bo@brilliant.tech>
2020-03-23 16:53:40 -0700

    Adding mesh dfu thrift types
      CR: Jeremy

---
+++ mesh_dfu.thrift
@@ -0,0 +1,10 @@
+namespace py thrift_types.mesh_dfu
+namespace java thrift_types.mesh_dfu
+
+enum MeshDfuHexType {
+  SOFTDEVICE = 1
+  BOOTLOADER = 2
+  SOFTDEVICE_BOOTLOADER = 3
+  APPLICATION = 4
+  MESH_BOOTLOADER_INFO = 8
+}
