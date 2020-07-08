069301f446ea862c326a471fad712d4f02be71ad
Aman Dhar <aman@brilliant.tech>
2020-07-07 18:20:41 -0700

    Add climate sensor interface thrift.
    CR: Ananda

---
+++ peripheral_interfaces/climate_sensor_interface.thrift
@@ -0,0 +1,19 @@
+namespace py thrift_types.peripheral_interfaces.climate_sensor_interface
+namespace java thrift_types.peripheral_interfaces.climate_sensor_interface
+
+include "../climate_sensor.thrift"
+include "../configuration.thrift"
+include "../message_bus.thrift"
+include "peripheral_interface.thrift"
+
+struct ClimateSensorPeripheralInterface {
+  1: configuration.RoomAssignment room_assignment
+  2: i32 ambient_temperature_f
+  3: double ambient_humidity // percentage in interval [0, 100]
+  4: climate_sensor.ClimateAlarms climate_alarms
+}
+
+const peripheral_interface.PeripheralInterface peripheral = {
+  'peripheral_interface_name': 'ClimateSensorPeripheralInterface',
+  'peripheral_type': message_bus.PeripheralType.CLIMATE_SENSOR
+}
