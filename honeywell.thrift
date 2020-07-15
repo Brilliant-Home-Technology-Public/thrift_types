c1c6e4fe6b8f639bc07d6a1f18560b5ec178b1a7
Aman Dhar <aman@brilliant.tech>
2020-07-15 15:54:38 -0700

    Adding constants for leak detector alarms.
    CR: Ananda

---
+++ honeywell.thrift
@@ -0,0 +1,10 @@
+namespace py thrift_types.honeywell
+namespace java thrift_types.honeywell
+
+// Alarm types received from Honeywell leak detectors
+const string HONEYWELL_HIGH_TEMPERATURE_ALARM = "HighTemperature"
+const string HONEYWELL_LOW_TEMPERATURE_ALARM = "LowTemperature"
+const string HONEYWELL_HIGH_HUMIDITY_ALARM = "HighHumidity"
+const string HONEYWELL_WATER_LEAK_ALARM = "WaterLeak"
+const string HONEYWELL_LOW_BATTERY_ALARM = "LowBattery"
+const string HONEYWELL_CRITICAL_BATTERY_ALARM = "CriticalBattery"
