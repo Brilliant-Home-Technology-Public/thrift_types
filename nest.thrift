52ecd75fb21e8ec73eca0227f75e8d5b3fdd2e29
Aman Dhar <aman@brilliant.tech>
2020-08-13 17:59:46 -0700

    Add minimum API version needed to support linking new Nest accounts to Brilliant.
    - Version 1 is pre-2019, version 2 is most recent
    
    CR: Ananda, Ted

--- nest.thrift
+++ nest.thrift
@@ -5,6 +5,7 @@ const string NEST_API_BASE = "https://developer-api.nest.com"
 // Required min difference between heat and cool temps in heat-cool mode
 const i32 HEAT_COOL_MIN_DELTA = 3
 const string NEST_DEMO_ACCESS_TOKEN = "NEST_DEMO_ACCESS_TOKEN"
+const i32 GOOGLE_NEST_SUPPORT_MIN_API_VERSION = 2
 
 struct ThermostatIds {
 	1: list<string> thermostat_ids
