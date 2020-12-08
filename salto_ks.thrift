de0b4294d4987fa07e841050c47c25c8845e4d76
Aman Dhar <aman@brilliant.tech>
2020-12-07 17:24:07 -0800

    Add SALTO_OTP_INVALID_ERROR_CODE constant.
    CR: Ananda

--- salto_ks.thrift
+++ salto_ks.thrift
@@ -1,6 +1,8 @@
 namespace py thrift_types.salto_ks
 namespace java thrift_types.salto_ks
 
+const string SALTO_OTP_INVALID_ERROR_CODE = "otp_invalid"
+
 enum IQAuthState {
 	NEVER_ATTEMPTED = 0,
 	AUTHORIZED = 1,
