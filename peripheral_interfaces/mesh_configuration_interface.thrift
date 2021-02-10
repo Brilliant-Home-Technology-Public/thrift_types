6b745169ac09b644f0ca49296e30af692ebe9d1e
Patrick Mogan <patrick@brilliant.tech>
2021-02-09 17:32:40 -0800

    [peripheral_interface] Add optional allow_background_dfu field to MeshConfigurationInterface
    CR: Jeremy

--- peripheral_interfaces/mesh_configuration_interface.thrift
+++ peripheral_interfaces/mesh_configuration_interface.thrift
@@ -15,6 +15,7 @@ struct MeshConfigurationPeripheralInterface {
   6: string relay_arbiter_device_id
   7: i32 next_unicast_address
   8: optional bluetooth.TrackedReleaseStages tracked_release_stages
+  9: optional bool allow_background_dfu
 }
 
 const peripheral_interface.PeripheralInterface peripheral = {
