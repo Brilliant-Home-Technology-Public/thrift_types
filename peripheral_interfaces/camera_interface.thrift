1e81a28d55cf43c1a205e1d9806734c881f1da11
Ananda Bose <ananda@brilliant.tech>
2021-04-01 17:00:57 -0700

    Add remote media sessions to Camera thrift peripheral interface.
    CR: Aman, Ted

--- peripheral_interfaces/camera_interface.thrift
+++ peripheral_interfaces/camera_interface.thrift
@@ -4,13 +4,17 @@ namespace java thrift_types.peripheral_interfaces.camera_interface
 include "../configuration.thrift"
 include "../message_bus.thrift"
 include "peripheral_interface.thrift"
+include "../remote_media.thrift"
 
 struct CameraPeripheralInterface {
   1: string display_name
   2: configuration.RoomAssignment room_assignment
+  3: remote_media.RemoteMediaSessions remote_sessions
 }
 
 const peripheral_interface.PeripheralInterface peripheral = {
   'peripheral_interface_name': 'CameraPeripheralInterface',
   'peripheral_type': message_bus.PeripheralType.CAMERA,
+  'dynamic_variable_prefix': remote_media.LIVE_VIEW_SESSION_PREFIX,
+  'dynamic_variable_ttype': 'remote_media.RemoteMediaSession'
 }
