3015c6c56b31f8a26a664290008dadffa2c29985
iva2k <iva2k@yahoo.com>
2021-04-02 19:11:26 -0700

    Add configuration variables for DFU fixes

--- mesh_dfu.thrift
+++ mesh_dfu.thrift
@@ -32,6 +32,7 @@ struct MeshUpdateStateInfo {
   10: optional i32 last_sent_packet_index
   // The last index of the update packet stream that the state machine intentionally skips
   11: optional i32 last_skipped_packet_index
+  12: bool ignore_missing_skip_packet
 }
 
 struct MeshUpdateStates {
