4c107475bc514ae0bf581757625d650243247157
Ananda Bose <ananda@brilliant.tech>
2021-08-10 15:50:10 -0700

    Add peripheral interface for Honeywell TC2 Configuration
    https://brillianthome.atlassian.net/browse/CQ-5276
    
    CR: Aman

---
+++ peripheral_interfaces/honeywell_tc2_configuration_interface.thrift
@@ -0,0 +1,21 @@
+namespace py thrift_types.peripheral_interfaces.honeywell_tc2_configuration_interface
+namespace java thrift_types.peripheral_interfaces.honeywell_tc2_configuration_interface
+
+include "../configuration.thrift"
+include "../message_bus.thrift"
+include "peripheral_interface.thrift"
+
+
+struct HoneywellTC2ConfigurationPeripheralInterface {
+    1: string access_token
+    2: i64 expiration_date
+    4: string owner
+    5: configuration.ThirdpartyIntegrationState thirdparty_integration_state
+}
+
+const peripheral_interface.PeripheralInterface peripheral = {
+  'peripheral_interface_name': 'HoneywellTC2ConfigurationPeripheralInterface',
+  'peripheral_type': message_bus.PeripheralType.HONEYWELL_TC2_CONFIGURATION,
+  'dynamic_variable_prefix': message_bus.PROCESS_CONFIGURATION_VARIABLE_PREFIX,
+  'dynamic_variable_ttype': 'configuration.PeripheralInfo'
+}
