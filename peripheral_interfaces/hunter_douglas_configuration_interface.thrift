b979017021f696000899c6af9e82d6d61cdcedb0
Ananda Bose <ananda@brilliant.tech>
2022-04-28 11:52:37 -0700

    Add thrift interface for HUNTER_DOUGLAS_CONFIGURATION peripheral.
    https://brillianthome.atlassian.net/browse/CQ-6629
    
    CR: Aman

---
+++ peripheral_interfaces/hunter_douglas_configuration_interface.thrift
@@ -0,0 +1,19 @@
+namespace py thrift_types.peripheral_interfaces.hunter_douglas_configuration_interface
+namespace java thrift_types.peripheral_interfaces.hunter_douglas_configuration_interface
+
+include "../configuration.thrift"
+include "../message_bus.thrift"
+include "peripheral_interface.thrift"
+
+
+struct HunterDouglasConfigurationPeripheralInterface {
+    1: string owner
+    2: configuration.ThirdpartyIntegrationState thirdparty_integration_state
+}
+
+const peripheral_interface.PeripheralInterface peripheral = {
+  'peripheral_interface_name': 'HunterDouglasConfigurationPeripheralInterface',
+  'peripheral_type': message_bus.PeripheralType.HUNTER_DOUGLAS_CONFIGURATION,
+  'dynamic_variable_prefix': message_bus.PROCESS_CONFIGURATION_VARIABLE_PREFIX,
+  'dynamic_variable_ttype': 'configuration.PeripheralInfo'
+}
