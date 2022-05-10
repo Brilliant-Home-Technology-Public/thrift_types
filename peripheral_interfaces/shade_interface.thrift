620818b7e582e507da4efe00cbc53a33820f6757
Ananda Bose <ananda@brilliant.tech>
2022-05-09 17:49:16 -0700

    Add new variables to ShadePeripheralInterface.
    https://brillianthome.atlassian.net/browse/CQ-6669
    
    CR: Aman

--- peripheral_interfaces/shade_interface.thrift
+++ peripheral_interfaces/shade_interface.thrift
@@ -2,6 +2,7 @@ namespace py thrift_types.peripheral_interfaces.shade_interface
 namespace java thrift_types.peripheral_interfaces.shade_interface
 
 include "../message_bus.thrift"
+include "../shade.thrift"
 include "peripheral_interface.thrift"
 
 struct ShadePeripheralInterface {
@@ -9,6 +10,11 @@ struct ShadePeripheralInterface {
   // positions or must be either all up or all down
   1: bool continuous
   2: i32 position
+
+  3: optional shade.ShadeCapabilities capabilities
+  4: optional i32 primary_position
+  5: optional i32 secondary_position
+  6: optional i32 tilt_position
 }
 
 const peripheral_interface.PeripheralInterface peripheral = {
