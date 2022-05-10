03b740f6edbbe832c33ecfbfa73be916b9037210
Ananda Bose <ananda@brilliant.tech>
2022-05-09 17:48:43 -0700

    Add new shade thrift_types
    https://brillianthome.atlassian.net/browse/CQ-6669
    
    CR: Aman

---
+++ shade.thrift
@@ -0,0 +1,18 @@
+namespace py thrift_types.shade
+namespace java thrift_types.shade
+
+enum PositionType {
+  PRIMARY = 1
+  SECONDARY = 2
+  TILT = 3
+}
+
+struct PositionCapability {
+  1: PositionType position_type
+  2: bool continuous
+  3: optional string display_name
+}
+
+struct ShadeCapabilities {
+  1: map<PositionType, PositionCapability> supported_positions
+}
