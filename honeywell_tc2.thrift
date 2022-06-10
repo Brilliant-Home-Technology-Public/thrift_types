52abc55236ea69d0046ac76d1a7d41d1dedb75d2
Ananda Bose <ananda@brilliant.tech>
2022-06-10 10:11:20 -0700

    Add event key for Honeywell TC2 Doorbell Press events.
    https://brillianthome.atlassian.net/browse/CQ-6781
    
    CR: Ted

--- honeywell_tc2.thrift
+++ honeywell_tc2.thrift
@@ -2,3 +2,4 @@ namespace py thrift_types.honeywell_tc2
 namespace java thrift_types.honeywell_tc2
 
 const string HONEYWELL_TC2_EVENT_KEY_SYSTEM_MODE = "honeywell_tc2:system_mode"
+const string HONEYWELL_TC2_EVENT_KEY_DOORBELL_PRESS = "honeywell_tc2:doorbell_press"
