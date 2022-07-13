4a771c2a240ad1e5766a6c556313def44ae70dec
Ananda Bose <ananda@brilliant.tech>
2022-07-13 15:07:37 -0700

    Add event key for system status polling.
    https://brillianthome.atlassian.net/browse/CQ-6956
    
    CR: Patrick

--- honeywell_tc2.thrift
+++ honeywell_tc2.thrift
@@ -3,3 +3,4 @@ namespace java thrift_types.honeywell_tc2
 
 const string HONEYWELL_TC2_EVENT_KEY_SYSTEM_MODE = "honeywell_tc2:system_mode"
 const string HONEYWELL_TC2_EVENT_KEY_DOORBELL_PRESS = "honeywell_tc2:doorbell_press"
+const string HONEYWELL_TC2_EVENT_KEY_POLL_SYSTEM_STATUS = "honeywell_tc2:poll_system_status"
