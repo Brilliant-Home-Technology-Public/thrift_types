24d58694a32b18d0e7c179204de8dc3f1e048322
Jae Young Lee <jaeyoung@brilliant.tech>
2022-07-29 13:48:52 -0700

    Add RECENTLY_PLAYED_URI_PREFIX for sonos constant
    CQ-7123
    
    CR: Jack & Ananda

--- sonos.thrift
+++ sonos.thrift
@@ -4,6 +4,7 @@ namespace java thrift_types.sonos
 // For favorites, set uri to "<raw_uri>". For playlists, set uri to "playlist:<raw_uri>"
 const string PLAYLIST_URI_PREFIX = "playlist"
 const string LINE_IN_URI_PREFIX = "line_in_source"
+const string RECENTLY_PLAYED_URI_PREFIX = "recently_played"
 // Set 'play_uri' variable to HOME_THEATER_PREFIX_URI to start Home Theater playback
 // PREFIX naming allows possibility of supporting Home Theater playback on a group where the
 // coordinator is not the Sonos with the Home Theater input.
