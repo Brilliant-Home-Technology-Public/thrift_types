c2f5261827b472f7c162014ed77b2adb70339281
alvin-yuan <alvin@brilliant.tech>
2022-08-29 14:56:47 -0700

    Update DispatchRequest to support proportional adjustments
    CR: Jackson

--- request_dispatcher.thrift
+++ request_dispatcher.thrift
@@ -3,6 +3,14 @@ namespace java thrift_types.request_dispatcher
 
 include "configuration.thrift"
 
+struct StatefulDispatchRequestDetails {
+  1: string context_id
+  // Static set of variables are whitelisted to support adjust_proportionally.
+  // For now, it will just be intensity.
+  2: bool adjust_proportionally
+}
+
 struct DispatchRequest {
   1: configuration.ActionSet action_set_intention
+  2: optional StatefulDispatchRequestDetails stateful_details
 }
