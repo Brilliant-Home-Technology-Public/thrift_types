81341e99897aea53ddf9e6389348f7bfef7644bb
Ananda Bose <ananda@brilliant.tech>
2023-01-10 18:20:30 -0800

    Add event keys for handling Honeywell TC2 Switch and SwitchLevel updates.
    https://brillianthome.atlassian.net/browse/CQ-7654
    
    CR: Melody

--- honeywell_tc2.thrift
+++ honeywell_tc2.thrift
@@ -4,3 +4,5 @@ namespace java thrift_types.honeywell_tc2
 const string HONEYWELL_TC2_EVENT_KEY_SYSTEM_MODE = "honeywell_tc2:system_mode"
 const string HONEYWELL_TC2_EVENT_KEY_DOORBELL_PRESS = "honeywell_tc2:doorbell_press"
 const string HONEYWELL_TC2_EVENT_KEY_POLL_SYSTEM_STATUS = "honeywell_tc2:poll_system_status"
+const string HONEYWELL_TC2_EVENT_KEY_SWITCH_STATE = "honeywell_tc2:switch_state"
+const string HONEYWELL_TC2_EVENT_KEY_SWITCH_LEVEL = "honeywell_tc2:switch_level"
