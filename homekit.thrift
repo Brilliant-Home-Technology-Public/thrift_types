ee173abf078f21f59a7624eab2950a34ed99fffe
alvin-yuan <alvin@brilliant.tech>
2023-01-13 16:39:46 -0800

    Add homekit PPID details
    JIRA: CQ-7837
    CR: Ashley

--- homekit.thrift
+++ homekit.thrift
@@ -17,3 +17,16 @@ struct SetupInfo {
   2: string setup_code
 }
 
+
+# These details can be found on mfi.apple.com.
+# Historically, we used the Jabil PPID for internally flashed devices.
+const string JABIL_FACTORY_HOMEKIT_AUTH_ENTITY_PPID = "244541-0004"
+const string VTECH_FACTORY_HOMEKIT_AUTH_ENTITY_PPID = "244541-171532"
+const map<string, map<string, string>> PRODUCT_PLAN_DETAILS_BY_PPID = {
+    JABIL_FACTORY_HOMEKIT_AUTH_ENTITY_PPID: {
+        "product_data": "01651fe302eb7eac"
+    },
+    VTECH_FACTORY_HOMEKIT_AUTH_ENTITY_PPID: {
+        "product_data": "01651fe3f3a8f263"
+    },
+}
