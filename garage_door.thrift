ed3871cb8ca534034e9f12f37797fcbe3ceb42de
melody008 <melody@brilliant.tech>
2023-02-01 12:29:58 -0800

    Add garage door thrift types and migration genie thrift types
    CQ-7987
    
    CR: Ananda

---
+++ garage_door.thrift
@@ -0,0 +1,17 @@
+namespace py thrift_types.garage_door
+namespace java thrift_types.garage_door
+
+
+const string GARAGE_DOOR_STATUS_OPEN = "open"
+const string GARAGE_DOOR_STATUS_OPENING = "opening"
+const string GARAGE_DOOR_STATUS_CLOSED = "closed"
+const string GARAGE_DOOR_STATUS_CLOSING = "closing"
+const string GARAGE_DOOR_STATUS_UNKNOWN = "unknown"
+
+const string GARAGE_DOOR_FAULT_NONE = "none"
+
+const string GARAGE_DOOR_DEVICE_STATUS_OFFLINE = "offline"
+const string GARAGE_DOOR_DEVICE_STATUS_ONLINE = "online"
+
+const string GARAGE_DOOR_COMMAND_OPEN = "open"
+const string GARAGE_DOOR_COMMAND_CLOSE = "close"
--- genie.thrift
+++ genie.thrift
@@ -1,6 +1,8 @@
 namespace py thrift_types.genie
 namespace java thrift_types.genie
 
+include "garage_door.thrift"
+
 // Event keys
 const string EVENT_KEY_DOOR_STATUS = "genie:door_status"
 const string EVENT_KEY_LINK_STATUS = "genie:link_status"
@@ -10,20 +12,20 @@ const string EVENT_KEY_FAULT = "genie:fault"
 // These are constants for the Genie API
 // https://brillianthome.atlassian.net/wiki/spaces/TP/pages/193429539/Genie+API
 
-const string DOOR_STATUS_OPEN = "open"
-const string DOOR_STATUS_OPENING = "opening"
-const string DOOR_STATUS_CLOSED = "closed"
-const string DOOR_STATUS_CLOSING = "closing"
-const string DOOR_STATUS_UNKNOWN = "unknown"
+const string DOOR_STATUS_OPEN = "open" // Being migrated to garage_door.thrift
+const string DOOR_STATUS_OPENING = "opening" // Being migrated to garage_door.thrift
+const string DOOR_STATUS_CLOSED = "closed" // Being migrated to garage_door.thrift
+const string DOOR_STATUS_CLOSING = "closing" // Being migrated to garage_door.thrift
+const string DOOR_STATUS_UNKNOWN = "unknown" // Being migrated to garage_door.thrift
 const string DOOR_STATUS_TIMEOUT_OPENING = "timeout_opening"
 const string DOOR_STATUS_TIMEOUT_CLOSING = "timeout_closing"
 const map<i32, string> DOOR_STATUS_ENUM_TO_STRING = {
-     0: DOOR_STATUS_UNKNOWN,
-     1: DOOR_STATUS_OPEN,
-     2: DOOR_STATUS_OPENING,
+     0: garage_door.GARAGE_DOOR_STATUS_UNKNOWN,
+     1: garage_door.GARAGE_DOOR_STATUS_OPEN,
+     2: garage_door.GARAGE_DOOR_STATUS_OPENING,
      3: DOOR_STATUS_TIMEOUT_OPENING,
-     4: DOOR_STATUS_CLOSED,
-     5: DOOR_STATUS_CLOSING,
+     4: garage_door.GARAGE_DOOR_STATUS_CLOSED,
+     5: garage_door.GARAGE_DOOR_STATUS_CLOSING,
      6: DOOR_STATUS_TIMEOUT_CLOSING,
 }
 
@@ -38,24 +40,24 @@ const map<i32, string> DOOR_LINK_STATUS_ENUM_TO_STRING = {
      3: DOOR_LINK_STATUS_CONNECTED,
 }
 
-const string DOOR_FAULT_NONE = "none"
+const string DOOR_FAULT_NONE = "none" // Being migrated to garage_door.thrift
 const string DOOR_FAULT_UL_LOCKOUT = "ul_lockout"
 const string DOOR_FAULT_MOVE_ABORT = "move_abort"
 const string DOOR_FAULT_UNKNOWN = "unknown"
 const map<i32, string> DOOR_FAULT_ENUM_TO_STRING = {
-     0: DOOR_FAULT_NONE,
+     0: garage_door.GARAGE_DOOR_FAULT_NONE,
      1: DOOR_FAULT_UL_LOCKOUT,
      2: DOOR_FAULT_MOVE_ABORT,
      3: DOOR_FAULT_UNKNOWN,
      4: DOOR_FAULT_UNKNOWN,
 }
 
-const string DEVICE_STATUS_OFFLINE = "offline"
-const string DEVICE_STATUS_ONLINE = "online"
+const string DEVICE_STATUS_OFFLINE = "offline" // Being migrated to garage_door.thrift
+const string DEVICE_STATUS_ONLINE = "online" // Being migrated to garage_door.thrift
 const map<i32, string> DEVICE_STATUS_ENUM_TO_STRING = {
-     0: DEVICE_STATUS_OFFLINE,
-     1: DEVICE_STATUS_ONLINE,
+     0: garage_door.GARAGE_DOOR_DEVICE_STATUS_OFFLINE,
+     1: garage_door.GARAGE_DOOR_DEVICE_STATUS_ONLINE,
 }
 
-const string DOOR_COMMAND_OPEN = "open"
-const string DOOR_COMMAND_CLOSE = "close"
+const string DOOR_COMMAND_OPEN = "open" // Being migrated to garage_door.thrift
+const string DOOR_COMMAND_CLOSE = "close" // Being migrated to garage_door.thrift
