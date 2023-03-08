3e4c85f0154e570a2d8f9952366c1eddfc0a31da
Patrick Mogan <patrick@brilliant.tech>
2023-03-07 16:23:10 -0800

    Add uart.thrift
    CR: Jeremy

---
+++ uart.thrift
@@ -0,0 +1,10 @@
+namespace py thrift_types.uart
+namespace java thrift_types.uart
+
+# Enum values (apart from UNKNOWN) match device_type values given by the "idnt" command
+enum UARTDeviceType {
+  UNKNOWN = -1
+  STANDARD_GANGBOX = 0
+  FACEPLATE = 1
+  LOADLESS_GANGBOX = 2
+}
