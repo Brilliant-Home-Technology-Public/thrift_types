32aa66b38b5d07ceaace5ff1dc935446050208cf
Patrick Mogan <patrick@brilliant.tech>
2023-03-14 12:00:36 -0700

    fixup! uart.thrift: Change UARTDeviceType UNKNOWN enum value to 2^16

--- uart.thrift
+++ uart.thrift
@@ -1,12 +1,11 @@
 namespace py thrift_types.uart
 namespace java thrift_types.uart
 
-# XXX: This constant represents another UARTDeviceType, but Swift and Java compilers generate a
-# enum identifier representing -1 that would conflict with UARTDeviceType's equivalent identifier
-const i32 UNKNOWN_UART_DEVICE_TYPE = -1
-
-# Enum values match device_type values given by the "idnt" command
+# Enum values (apart from UNKNOWN) match device_type values given by the "idnt" command
 enum UARTDeviceType {
+  # XXX: We use 2^16 here instead of -1 because our Swift and Java compilers generate an enum
+  # identifier representing -1 that would conflict with UARTDeviceType's equivalent identifier
+  UNKNOWN = 65536
   GANGBOX = 0
   FACEPLATE = 1
   LOADLESS_BASE = 2
