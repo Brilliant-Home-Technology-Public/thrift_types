3a0a46f41a173af894b7485e4b5fd9bd3b6c65a7
Patrick Mogan <patrick@brilliant.tech>
2023-04-25 10:49:33 -0700

    gangbox.thrift: Document NO_EASING and LINEAR easing types
    CR: Alvin

--- gangbox.thrift
+++ gangbox.thrift
@@ -2,7 +2,14 @@ namespace py thrift_types.gangbox
 namespace java thrift_types.gangbox
 
 enum EasingType {
+  // Jump immediately to requested intensity values. This is useful during magnetic load detection
+  // to ensure that the load is not set to any intermediate intensity levels.
   NO_EASING = 0,
+  // Ease into requested intensity values at a linear rate. For a control gangbox using the
+  // LINEAR EasingType, we'll still smooth values for set requests that only include intensity
+  // even if the fade rate is 0. This is to make the experience of using sliders (which
+  // throttle intensity sets) when the load is already on feel smooth at this fade rate. Hence
+  // this is different from NO_EASING, which never smooths any intensity sets.
   LINEAR = 1,
   EASE_OUT_CUBIC = 2,
 }
@@ -16,6 +23,7 @@ enum MinDimlevelCalibrationStatus {
 struct DimSmoothingConfiguration {
   1: EasingType easing_type
   // Required for all easing types except for NO_EASING
+  //   - Switch, however, expects step_interval_ms and min_step_magnitude to be None
   2: optional i32 step_interval_ms
   3: optional i32 min_step_magnitude
   4: optional i32 max_fade_in_duration_ms
