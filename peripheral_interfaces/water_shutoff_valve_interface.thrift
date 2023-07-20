d8c9edfdfab22f27ba7dd87330cca7c019c7dc5c
Jaeyoung Lee <jaeyoung@brilliant.tech>
2023-07-19 17:20:57 -0700

    Add water_shutoff_valve_interface.thrift to support an interface for a shutoff valve peripheral
    CR: Ananda

---
+++ peripheral_interfaces/water_shutoff_valve_interface.thrift
@@ -0,0 +1,18 @@
+namespace py thrift_types.peripheral_interfaces.water_shutoff_valve_interface
+namespace java thrift_types.peripheral_interfaces.water_shutoff_valve_interface
+
+include "../configuration.thrift"
+include "../message_bus.thrift"
+include "../water_shutoff_valve.thrift"
+include "peripheral_interface.thrift"
+
+struct WaterShutoffValvePeripheralInterface {
+  1: string display_name
+  2: configuration.RoomAssignment room_assignment
+  3: water_shutoff_valve.WATER_SHUTOFF_VALVE_STATUS valve_status
+}
+
+const peripheral_interface.PeripheralInterface peripheral = {
+  'peripheral_interface_name': 'WaterShutoffValvePeripheralInterface',
+  'peripheral_type': message_bus.PeripheralType.WATER_SHUTOFF_VALVE
+}
