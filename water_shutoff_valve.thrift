b2e3f4c74a90bd46dc4ae4a66f9ad7cd5b1481ba
Jaeyoung Lee <jaeyoung@brilliant.tech>
2023-07-19 17:19:45 -0700

    Add water_shutoff_valve.thrift to support shutoff valve states
    CQ-8864
    
    CR: Ananda

---
+++ water_shutoff_valve.thrift
@@ -0,0 +1,10 @@
+namespace py thrift_types.water_shutoff_valve
+namespace java thrift_types.water_shutoff_valve
+
+enum WATER_SHUTOFF_VALVE_STATUS {
+	UNKNOWN = 0
+	OPEN = 1
+	CLOSE = 2
+	OPEN_FAILED = 3
+	CLOSE_FAILED = 4
+}
