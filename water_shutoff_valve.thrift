78cfbfdc0c6257977e1979aab3e1042835e5e051
Jaeyoung Lee <jaeyoung@brilliant.tech>
2023-08-21 14:53:43 -0700

    Update the format in water_shutoff_valve thrift
    CR: Ananda

--- water_shutoff_valve.thrift
+++ water_shutoff_valve.thrift
@@ -2,11 +2,11 @@ namespace py thrift_types.water_shutoff_valve
 namespace java thrift_types.water_shutoff_valve
 
 enum WaterShutoffValveStatus {
-	UNKNOWN = 0
-	OPEN = 1
-	CLOSE = 2
-	OPEN_FAILED = 3
-	CLOSE_FAILED = 4
+	UNKNOWN = 0,
+	OPEN = 1,
+	CLOSE = 2,
+	OPEN_FAILED = 3,
+	CLOSE_FAILED = 4,
 }
 
 enum WaterLeakStatus {
