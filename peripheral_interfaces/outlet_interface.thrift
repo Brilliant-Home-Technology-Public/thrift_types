211ff4cfc7d7d11f51fb223e6d06eb3c81b23d07
alvin-yuan <alvin@brilliant.tech>
2023-11-02 17:47:18 -0700

    Add room_assignment to OUTLET interface
    Similar to c16359a. This is important to fix
    switch cap touch gestures to work with groups.
    
    CR: Jackson

--- peripheral_interfaces/outlet_interface.thrift
+++ peripheral_interfaces/outlet_interface.thrift
@@ -1,11 +1,13 @@
 namespace py thrift_types.peripheral_interfaces.outlet_interface
 namespace java thrift_types.peripheral_interfaces.outlet_interface
 
+include "../configuration.thrift"
 include "../message_bus.thrift"
 include "peripheral_interface.thrift"
 
 struct OutletPeripheralInterface {
     1: bool on
+    2: configuration.RoomAssignment room_assignment
 }
 
 const peripheral_interface.PeripheralInterface peripheral = {
