aab6667f412d2fb8e01dc52ad4c87a1339dbeaf3
Niko  Lillios <niko@brilliant.tech>
2023-11-09 16:00:57 -0800

    Fix definition order of solar thrift structs
    CR: Ted

--- solar.thrift
+++ solar.thrift
@@ -1,17 +1,17 @@
 namespace py thrift_types.solar
 namespace java thrift_types.solar
 
+enum HomeHeatingType {
+  UNKNOWN = 0,
+  NONE = 1,
+  GAS = 2,
+  ELECTRIC = 3,
+}
+
 struct SolarConfiguration {
   1: i32 average_monthly_energy_bill_dollars
   2: double solar_array_size_kw
   3: HomeHeatingType heating_type
   4: bool has_ev
   5: i32 projected_percent_energy_from_solar
-}
-
-enum HomeHeatingType {
-  UNKNOWN = 0,
-  NONE = 1,
-  GAS = 2,
-  ELECTRIC = 3,
 }
