757cc4d680c71a0412ef7cc1fcb63dfc8c1a2ab2
Jaeyoung Lee <jaeyoung@brilliant.tech>
2023-12-12 12:30:36 -0800

    Add Low Humidity Alert thrift types
    CR: Ananda

--- honeywell.thrift
+++ honeywell.thrift
@@ -5,6 +5,7 @@ namespace java thrift_types.honeywell
 const string HONEYWELL_HIGH_TEMPERATURE_ALARM = "HighTemperature"
 const string HONEYWELL_LOW_TEMPERATURE_ALARM = "LowTemperature"
 const string HONEYWELL_HIGH_HUMIDITY_ALARM = "HighHumidity"
+const string HONEYWELL_LOW_HUMIDITY_ALARM = "LowHumidity"
 const string HONEYWELL_WATER_LEAK_ALARM = "WaterLeak"
 const string HONEYWELL_LOW_BATTERY_ALARM = "LowBattery"
 const string HONEYWELL_CRITICAL_BATTERY_ALARM = "CriticalBattery"
