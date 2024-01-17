1a099f5ae797d05de1aa11b76835c1a05e3e6374
Niko  Lillios <niko@brilliant.tech>
2024-01-17 13:33:25 -0800

    Solar: Add SolarSavings struct
    CR: Ashley

--- solar.thrift
+++ solar.thrift
@@ -1,6 +1,8 @@
 namespace py thrift_types.solar
 namespace java thrift_types.solar
 
+include "configuration.thrift"
+
 enum HomeHeatingType {
   UNKNOWN = 0,
   NONE = 1,
@@ -14,4 +16,10 @@ struct SolarConfiguration {
   3: optional HomeHeatingType heating_type
   4: optional bool has_ev
   5: optional i32 projected_percent_energy_from_solar
+}
+
+struct SolarSavings {
+  1: i32 total_savings_dollars
+  2: configuration.CalendarDay start_day
+  3: configuration.CalendarDay end_day
 }
