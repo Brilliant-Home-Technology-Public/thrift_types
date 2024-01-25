4aa70b9f2b33ebcee47c461d9dd3d6e69b4b1803
Ananda Bose <ananda@brilliant.tech>
2024-01-25 13:10:09 -0800

    Add OFFLINE_LOCK support to client_ui mapping.
    No interactions are currently supported.
    
    https://brillianthome.atlassian.net/browse/CQ-10017
    
    CR: Kiyoshi

--- client_ui.thrift
+++ client_ui.thrift
@@ -230,6 +230,13 @@ const map<message_bus.PeripheralType, map<SupportType, bool>> CONTROL_PERIPHERAL
         SupportType.CAP_TOUCH: false,
         SupportType.MOTION: false
     },
+    message_bus.PeripheralType.OFFLINE_LOCK: {
+        SupportType.DEVICES: false,
+        SupportType.SCENES: false,
+        SupportType.GESTURES: false,
+        SupportType.CAP_TOUCH: false,
+        SupportType.MOTION: false
+    },
 }
 
 enum ControlLockScreenWidgetType {
