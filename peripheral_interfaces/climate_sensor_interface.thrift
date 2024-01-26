601ab7b472276b1522dd391af45c3ee76cc63b16
Jaeyoung Lee <jaeyoung@brilliant.tech>
2024-01-25 16:18:07 -0800

    Add battery_status in ClimateSensorPeripheralInterface
    CQ-9934
    
    CR: Ananda

--- peripheral_interfaces/climate_sensor_interface.thrift
+++ peripheral_interfaces/climate_sensor_interface.thrift
@@ -4,6 +4,7 @@ namespace java thrift_types.peripheral_interfaces.climate_sensor_interface
 include "../climate_sensor.thrift"
 include "../configuration.thrift"
 include "../message_bus.thrift"
+include "../battery.thrift"
 include "peripheral_interface.thrift"
 
 struct ClimateSensorPeripheralInterface {
@@ -11,6 +12,7 @@ struct ClimateSensorPeripheralInterface {
   2: i32 ambient_temperature_f
   3: double ambient_humidity // percentage in interval [0, 100]
   4: climate_sensor.ClimateAlarms climate_alarms
+  5: optional battery.BatteryStatus battery_status
 }
 
 const peripheral_interface.PeripheralInterface peripheral = {
