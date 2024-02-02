cf755e1999037c7b428a35d6fe46d8b68914c02b
Yuchen Zhang <yuchen@brilliant.tech>
2024-02-02 15:21:00 -0800

    Add Honeywell event keys.
    Jira: CQ-9963
    
    CR: Ananda

--- honeywell.thrift
+++ honeywell.thrift
@@ -12,3 +12,19 @@ const string HONEYWELL_CRITICAL_BATTERY_ALARM = "CriticalBattery"
 
 // Update Honeywell peripherals every 5 min
 const i32 HONEYWELL_POLL_INTERVAL_MS = 300000
+
+// Honeywell Notification Event Keys
+const string HONEYWELL_EVENT_KEY_HIGH_TEMPERATURE_ALERT = "honeywell:high_temperature_alert"
+const string HONEYWELL_EVENT_KEY_LOW_TEMPERATURE_ALERT = "honeywell:low_temperature_alert"
+const string HONEYWELL_EVENT_KEY_HIGH_HUMIDITY_ALERT = "honeywell:high_humidity_alert"
+const string HONEYWELL_EVENT_KEY_LOW_HUMIDITY_ALERT = "honeywell:low_humidity_alert"
+const string HONEYWELL_EVENT_KEY_DEVICE_OFFLINE_ALERT = "honeywell:device_offline_alert"
+const string HONEYWELL_EVENT_KEY_LOW_BATTERY_ALERT = "honeywell:low_battery_alert"
+const string HONEYWELL_EVENT_KEY_WATER_LEAK_ALERT = "honeywell:water_leak_alert"
+const string HONEYWELL_EVENT_KEY_FAN_MODE = "honeywell:fan_mode"
+const string HONEYWELL_EVENT_KEY_HVAC_MODE = "honeywell:hvac_mode"
+const string HONEYWELL_EVENT_KEY_AMBIENT_HUMIDITY = "honeywell:ambient_humidity"
+const string HONEYWELL_EVENT_KEY_AMBIENT_TEMPERATURE = "honeywell:ambient_temperature"
+const string HONEYWELL_EVENT_KEY_UNITS = "honeywell:units"
+const string HONEYWELL_EVENT_KEY_HEAT_SETPOINT = "honeywell:heat_setpoint"
+const string HONEYWELL_EVENT_KEY_COOL_SETPOINT = "honeywell:cool_setpoint"
