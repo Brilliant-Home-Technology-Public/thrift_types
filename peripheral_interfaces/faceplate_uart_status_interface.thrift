c005f9194586467d9a4cfbb512c0ef118955ea9d
Patrick Mogan <patrick@brilliant.tech>
2024-03-19 11:40:55 -0700

    Add faceplate_uart_status_interface.thrift
    CR: Ashley

---
+++ peripheral_interfaces/faceplate_uart_status_interface.thrift
@@ -0,0 +1,18 @@
+namespace py thrift_types.peripheral_interfaces.faceplate_uart_status_interface
+namespace java thrift_types.peripheral_interfaces.faceplate_uart_status_interface
+
+include "../hardware.thrift"
+include "../message_bus.thrift"
+include "peripheral_interface.thrift"
+
+
+struct FaceplateUARTStatusPeripheralInterface {
+  1: hardware.FirmwareUpdateStatus firmware_update_status
+  2: string target_firmware_git_version
+  3: string reported_firmware_git_version
+}
+
+const peripheral_interface.PeripheralInterface peripheral = {
+  'peripheral_interface_name': 'FaceplateUARTStatusPeripheralInterface',
+  'peripheral_type': message_bus.PeripheralType.FACEPLATE_UART_STATUS,
+}
