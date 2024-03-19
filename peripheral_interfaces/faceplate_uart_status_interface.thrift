83ede13a63d898dd59e9c10b07112692b84e67bc
Patrick Mogan <patrick@brilliant.tech>
2024-03-19 12:04:37 -0700

    faceplate_uart_status_interface: Add device_revision to FaceplateUARTStatusPeripheralInterface
    CR: Ashley

--- peripheral_interfaces/faceplate_uart_status_interface.thrift
+++ peripheral_interfaces/faceplate_uart_status_interface.thrift
@@ -1,6 +1,7 @@
 namespace py thrift_types.peripheral_interfaces.faceplate_uart_status_interface
 namespace java thrift_types.peripheral_interfaces.faceplate_uart_status_interface
 
+include "../faceplate.thrift"
 include "../hardware.thrift"
 include "../message_bus.thrift"
 include "peripheral_interface.thrift"
@@ -10,6 +11,7 @@ struct FaceplateUARTStatusPeripheralInterface {
   1: hardware.FirmwareUpdateStatus firmware_update_status
   2: string target_firmware_git_version
   3: string reported_firmware_git_version
+  4: faceplate.FaceplateDeviceRevision device_revision
 }
 
 const peripheral_interface.PeripheralInterface peripheral = {
