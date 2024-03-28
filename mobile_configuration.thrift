275d9e488d46f0ce8ec21dcbf3a32e141887e1ba
idodra <haohua@brilliant.tech>
2024-03-28 16:59:10 -0700

    [Mobile] Updated mobile configuration Allegion Sentinels

--- mobile_configuration.thrift
+++ mobile_configuration.thrift
@@ -1,8 +1,9 @@
 namespace py thrift_types.mobile_configuration
 namespace java thrift_types.mobile_configuration
 
-const i64 ALLEGION_MOBILECREDENTIAL_DECLINED_SENTINEL = 1
-const i64 ALLEGION_MOBILECREDENTIAL_UNDETERMINED_SENTINEL = 2
+const i64 ALLEGION_MOBILECREDENTIAL_ACCEPTED_SENTINEL = -1
+const i64 ALLEGION_MOBILECREDENTIAL_DECLINED_SENTINEL = -2
+const i64 ALLEGION_MOBILECREDENTIAL_UNDETERMINED_SENTINEL = -3
 
 struct Profiles {
   1: list<string> profile_names
