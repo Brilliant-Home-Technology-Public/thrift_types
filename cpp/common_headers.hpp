52305869adca47d29991431b827a0a114057e228
Jeremy Hiatt <jeremy@brilliant.tech>
2024-04-16 13:46:07 -0700

    WIP define common header to support PCH

---
+++ cpp/common_headers.hpp
@@ -0,0 +1,17 @@
+#ifndef COMMON_HEADERS_HPP
+#define COMMON_HEADERS_HPP
+
+// This file includes other headers used across all generated C++ Thrift to serve
+// as an input to generate a pre-compiled header (PCH) package.
+
+#include <algorithm>
+#include <ostream>
+
+#include <thrift/TToString.h>
+#include <thrift/Thrift.h>
+#include <thrift/TApplicationException.h>
+#include <thrift/protocol/TProtocol.h>
+#include <thrift/transport/TTransport.h>
+
+#include <thrift/cxxfunctional.h>
+#endif // COMMON_HEADERS_HPP
