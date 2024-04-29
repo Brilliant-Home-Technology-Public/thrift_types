d49f58836a1333bca6491d640e248446367d2508
Yuchen Zhang <yuchen@brilliant.tech>
2024-04-29 12:50:14 -0700

    Added variables

---
+++ home_energy_system.thrift
@@ -0,0 +1,6 @@
+namespace py thrift_types.home_energy_system
+namespace java thrift_types.home_energy_system
+
+struct EnergyLifetime {
+    1: list<i32> energy_lifetime
+}
--- peripheral_interfaces/home_energy_system_interface.thrift
+++ peripheral_interfaces/home_energy_system_interface.thrift
@@ -3,6 +3,7 @@ namespace java thrift_types.peripheral_interfaces.home_energy_system_interface
 
 include "../configuration.thrift"
 include "../message_bus.thrift"
+include "../home_energy_system.thrift"
 include "peripheral_interface.thrift"
 
 struct HomeEnergySystemPeripheralInterface {
@@ -10,6 +11,9 @@ struct HomeEnergySystemPeripheralInterface {
   2: configuration.RoomAssignment room_assignment
   3: i32 solar_generation_today_wh
   4: i32 solar_consumption_today_wh
+  5: i32 energy_import_today_wh
+  6: home_energy_system.EnergyLifetime solar_consumption_lifetime_wh
+  7: home_energy_system.EnergyLifetime energy_import_lifetime_wh
 }
 
 const peripheral_interface.PeripheralInterface peripheral = {
